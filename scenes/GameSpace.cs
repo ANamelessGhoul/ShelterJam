82fdf7fc8fe2a14f1e2a8a426eed340df173a514
gokcankrc <gokcankrc@gmail.com>
2022-03-20 11:45:09 +0300

    Detect Speedup Map

--- scenes/GameSpace.cs
+++ scenes/GameSpace.cs
@@ -5,11 +5,13 @@ public class GameSpace : Node2D
 	public CharacterSelection CharacterSelection { get; private set; }
 	public Map ObstructionMap { get; private set; }
 	public Map SpeedupMap { get; private set; }
+	public Map VisualMap { get; private set; }
 
 	public override void _Ready()
 	{
 		SpeedupMap = GetNode<Map>("SpeedupMap");
 		ObstructionMap = GetNode<Map>("ObstructionMap");
+		VisualMap = GetNode<Map>("VisualMap");
 		CharacterSelection = GetNode<CharacterSelection>("CharacterSelection");
 	}
 

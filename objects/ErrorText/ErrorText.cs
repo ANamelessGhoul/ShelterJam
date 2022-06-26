21b24c6cc8107ba91bf68353989687de206fa316
Tecelli Eriz Akintug <tecelli.e.a@gmail.com>
2022-06-26 09:54:24 +0300

    Removed debug error text

--- objects/ErrorText/ErrorText.cs
+++ objects/ErrorText/ErrorText.cs
@@ -4,12 +4,10 @@ using System;
 public class ErrorText : Node
 {
     private PackedScene _errorLabelScene;
-    // Called when the node enters the scene tree for the first time.
+    
     public override void _Ready()
     {
         _errorLabelScene = GD.Load<PackedScene>("res://objects/ErrorText/ErrorLabel.tscn");
-
-        ShowErrorTextAtPosition("This is an Error Message!", new Vector2(120, 120));
     }
 
     public void ShowErrorTextAtPosition(string errorText, Vector2 worldPosition)
